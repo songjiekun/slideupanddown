c73b4215198f962ec9d35db8e44e4c99fe1e493b
song jie kun <songjiekun@hotmail.com>
2016-09-12 20:49:03 +0800

    Initial Commit

---
+++ testinterface/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testinterface
+//
+//  Created by song jiekun on 16/9/12.
+//  Copyright © 2016年 song jiekun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
